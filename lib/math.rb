8d98fc55e2d0f81838f760c07094e8a99840eac1
Jackson Beytebiere <jacksongrowson@gmail.com>
2020-07-30 22:03:13 +0000

    Automatically backed up by Learn

--- lib/math.rb
+++ lib/math.rb
@@ -1,23 +1,28 @@
 def addition(num1, num2)
-
+ans=num1+num2
+puts ans
 end
 
 def subtraction(num1, num2)
-
+ans=num1-num2
+puts ans
 end
 
 def division(num1, num2)
-
+ans=num1/num2
+puts ans
 end
 
 def multiplication(num1, num2)
-
+ans=num1*num2
+puts ans
 end
 
 def modulo(num1, num2)
-
+ans=num1%num2
+puts ans
 end
 
 def square_root(num)
-
+ans=
 end
